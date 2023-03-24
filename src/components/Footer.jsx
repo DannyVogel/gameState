5d2ae60f69386a88afeb72ce8c21b299a88f21c3
DannyVogel <danny.hj.vogel@gmail.com>
2023-03-24 16:05:54 +0100

    rearrange icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className='footerContainer'>
-        <img className='footerIcon' src="./gamesPlayed1.png" alt="" />
-        <img className='footerIcon searchIcon' src="./search.png" alt="" />
         <img className='footerIcon' src="./gamesToPlay1.png" alt="" />
+        <img className='footerIcon searchIcon' src="./search.png" alt="" />
+        <img className='footerIcon' src="./gamesPlayed1.png" alt="" />
     </div>
   )
 }
