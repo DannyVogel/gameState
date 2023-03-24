4df7b791452ca8ef1f03069fbdc60c6a744a37d0
DannyVogel <danny.hj.vogel@gmail.com>
2023-03-24 15:26:14 +0100

    create footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='footerContainer'>
+        <img className='footerIcon' src="./gamesPlayed1.png" alt="" />
+        <img className='footerIcon' src="./search.png" alt="" />
+        <img className='footerIcon' src="./gamesToPlay1.png" alt="" />
+    </div>
+  )
+}
+
+{/* <a href="https://www.flaticon.com/free-icons/want" title="want icons">Want icons created by redempticon - Flaticon</a> */}
+{/* <a href="https://www.flaticon.com/free-icons/gamification" title="gamification icons">Gamification icons created by redempticon - Flaticon</a> */}
+{/* <a href="https://www.flaticon.com/free-icons/gamepad" title="gamepad icons">Gamepad icons created by Freepik - Flaticon</a> */}
