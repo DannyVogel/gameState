2c94b2fe324c17a7fa6306c6594f94e11c7b9392
DannyVogel <danny.hj.vogel@gmail.com>
2023-03-24 11:29:09 +0100

    update search result component with result details

---
+++ src/components/GameSearchResult.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+
+export default function GameSearchResult(props) {
+  
+  console.log(props.result)
+    
+  return (
+    <div key={props.result.id} className="resultContainer">
+      <h3>
+        <a
+          className="resultLink"
+          href={`https://www.rawg.io/games/${props.result.slug}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          {props.result.name}{" "}
+        </a>
+      </h3>
+
+      <div className="midResultContainer">
+        {props.result.image && (
+          <img
+            className="resultImage"
+            src={props.result.image}
+            alt={props.result.name}
+          />
+        )}
+        <div className="resultInfoContainer">
+          <p className="resultText">Released: {props.result.released}</p>
+          <p className="resultText">
+            Genres: {props.result.genres.map((genre) => genre.name).join(", ")}
+          </p>
+          <a
+            className="resultText"
+            href={`https://www.rawg.io/games/${props.result.slug}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            More Info
+          </a>
+        </div>
+      </div>
+
+      <div className="endResultContainer">
+        <button
+          className="resultButton green btn" /*onClick={() => props.handleAddGame(props.result)}*/
+        >
+          I played it
+        </button>
+        <button
+          className="resultButton red btn" /*onClick={() => props.handleAddGame(props.result)}*/
+        >
+          I want to play it
+        </button>
+      </div>
+
+    </div>
+  );
+}
