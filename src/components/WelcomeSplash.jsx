8d76d41ce169c76988822375c9cb2ba2c9633d73
DannyVogel <danny.hj.vogel@gmail.com>
2023-03-24 23:04:46 +0100

    add animation to initial site load

--- src/components/WelcomeSplash.jsx
+++ src/components/WelcomeSplash.jsx
@@ -1,24 +1,27 @@
 import React from 'react'
+import { Fade } from "react-awesome-reveal";
 
 export default function WelcomeSplash() {
   return (
     <div className='welcomeContainer'>
-        <div className="iconContainer">
-            <h1 className='headerTitle'>Find Games!</h1>
-            <img className='footerIcon' src="./search.png" alt="magnifying glass search icon" />
-        </div>
-        <h1 className='headerTitle'>Save them to a list!</h1>
-        <div className='listsContainer'>
+        <Fade cascade={true} duration={1500} delay={500}>
             <div className="iconContainer">
-                <img className='footerIcon' src="./gamesToPlay1.png" alt="shopping bag with controller icon" />
-                <p className='headerTitle'>Games <span>{<br/>}</span> to play</p>
+                <h1 className='headerTitle'>Find Games!</h1>
+                <img className='footerIcon' src="./search.png" alt="magnifying glass search icon" />
             </div>
-            <div className="iconContainer">
-                <img className='footerIcon' src="./gamesPlayed1.png" alt="papers with controller icon" />
-                <p className='headerTitle'>Games <span>{<br/>}</span> played</p>
+            <h1 className='headerTitle'>Save them to a list!</h1>
+            <div className='listsContainer'>
+                <div className="iconContainer">
+                    <img className='footerIcon' src="./gamesToPlay1.png" alt="shopping bag with controller icon" />
+                    <p className='headerTitle'>Games <span>{<br/>}</span> to play</p>
+                </div>
+                <div className="iconContainer">
+                    <img className='footerIcon' src="./gamesPlayed1.png" alt="papers with controller icon" />
+                    <p className='headerTitle'>Games <span>{<br/>}</span> played</p>
+                </div>
             </div>
-        </div>
-        <h1 className='headerTitle'>Sign up to see your <span>{<br/>}</span> lists across devices!</h1>
+            <h1 className='headerTitle'>Sign up to see your <span>{<br/>}</span> lists across devices!</h1>
+        </Fade>
     </div>
   )
 }
