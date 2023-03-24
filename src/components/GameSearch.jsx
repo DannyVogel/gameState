5923373edde0c7c86c8ce3273fe4445b6aa2dc43
DannyVogel <danny.hj.vogel@gmail.com>
2023-03-24 12:52:25 +0100

    add functionality for showing 3 results at a time and switching pages

--- src/components/GameSearch.jsx
+++ src/components/GameSearch.jsx
@@ -5,6 +5,7 @@ import Result from '../utility/resultsConstructor'
 export default function GameSearch() {
   const [searchTerm, setSearchTerm] = useState('')
   const [results, setResults] = useState([])
+  const [num, setNum] = useState(0)
   const apiKey = 'b0bf8fc68ea6427fab93a3535a4be903'
   
   function handleChange(e){
@@ -18,26 +19,39 @@ export default function GameSearch() {
         .then(response => response.json())
         .then(data => {
             setResults(data.results.map(result => new Result(result)))
+            console.log(data.results)
         })
     }
 
-    const searchResultsEl = results.map(result => {
-        return <GameSearchResult 
-                    key={result.id} 
-                    result={result}
-                />
-    })
+    function renderFiveResults(results){
+      return results.map((result, index) => {  
+        console.log(index, num, (num + 3))  
+        if(index >= (num + 0) && index < (num + 3)){
+              return <GameSearchResult 
+                          key={result.id} 
+                          result={result}
+                      />
+        }})
+    }
 
   return (
     <div className='gameSearchContainer'>
-        <h1 className='headerTitle'><span className="gameItalic">game</span>Search</h1>
-        <form className='searchForm' onSubmit={processSearch}>
-            <input className='searchBar' type="text" id='searchTerm' name='searchTerm' value={searchTerm} onChange={handleChange} placeholder='Find your game' autoComplete='off'/>
-            <button className='searchBtn btn' >Search</button>
-        </form>
-        <div className='resultsContainer'>
-            {results.length > 0 ? searchResultsEl : null}
+      <h1 className='headerTitle'><span className="gameItalic">game</span>Search</h1>
+      <form className='searchForm' onSubmit={processSearch}>
+          <input className='searchBar' type="text" id='searchTerm' name='searchTerm' value={searchTerm} onChange={handleChange} placeholder='Find your game' autoComplete='off'/>
+          <button className='searchBtn btn'>Search</button>
+      </form>
+      <div className='resultsContainer'>
+          {results.length > 0 ? renderFiveResults(results) : null}
+      </div>
+      {results.length > 0 
+      ? <div className="pageSelectContainer">
+            {num > 0 ? <p className='pageSelectText' onClick={()=>setNum(prevNum => prevNum -= 3)}>Previous Page</p> : null}
+            {(num > 0 && num < results.length - 3) && <p className='pageSelectText'>-</p>}
+            {num < results.length - 3 && <p className='pageSelectText' onClick={()=>setNum(prevNum => prevNum += 3)}>Next Page</p>}
         </div>
+      : null
+      }
     </div>
   )
 }
