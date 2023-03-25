4bad7141a2bf62467022ed90e495005a7a0d0aab
DannyVogel <danny.hj.vogel@gmail.com>
2023-03-25 22:07:36 +0100

    add render game cards from database lists function

--- src/components/GamesPlayedList.jsx
+++ src/components/GamesPlayedList.jsx
@@ -1,23 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import GameCard from './GameCard'
+import {db, gameStateDB, ref, update, remove, onValue} from '../firebaseConfig'
 
-export default function GamesPlayedList() {
+export default function GamesPlayedList(props) {
+  const [savedList, setSavedList] = useState(()=>[])
+
+  useEffect(() => {
+    const gameRef = ref(db, `gameState/users/${props.userUID}/gamesPlayedList`)
+    onValue(gameRef, (snapshot) => {
+      snapshot.exists() ? setSavedList(Object.values(snapshot.val())) : null
+      console.log(Object.values(snapshot.val()))
+    })
+  }, []);
   
-  
-  function renderFiveResults(results){
-    return results.map((result, index) => {  
-      if(index >= (num + 0) && index < (num + 3)){
-            return <GameSearchResult 
-                        key={result.id} 
-                        result={result}
-                        handleAddGameToList={addGameToList}
+  function renderList(list){
+    return list.map((game) => {  
+            return <GameCard 
+                        key={game[0].id} 
+                        result={game[0]}
                     />
-      }})
+      })
   }
   
   return (
     <div>
-      <GameSearchResult />
+      {renderList(savedList)}
     </div>
   )
 }
