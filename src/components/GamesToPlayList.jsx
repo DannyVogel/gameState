fa4ad33ece47cf29b53075cf87ae25dd1ea00b21
DannyVogel <danny.hj.vogel@gmail.com>
2023-03-25 22:15:15 +0100

    create code for games to play list

--- src/components/GamesToPlayList.jsx
+++ src/components/GamesToPlayList.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import GameCard from './GameCard'
+import {db, ref, remove, onValue} from '../firebaseConfig'
 
-export default function GamesToPlayList() {
+export default function GamesToPlayList(props) {
+  const [savedList, setSavedList] = useState(()=>[])
+
+  useEffect(() => {
+    const gameRef = ref(db, `gameState/users/${props.userUID}/gamesToPlayList`)
+    onValue(gameRef, (snapshot) => {
+      snapshot.exists() ? setSavedList(Object.values(snapshot.val())) : null
+      console.log(Object.values(snapshot.val()))
+    })
+  }, []);
+  
+  function renderList(list){
+    return list.map((game) => {  
+            return <GameCard 
+                        key={game[0].id} 
+                        result={game[0]}
+                    />
+      })
+  }
+  
   return (
-    <div>GamesToPlayList</div>
+    <div className='gameSearchContainer'>
+      {renderList(savedList)}
+    </div>
   )
 }
+
