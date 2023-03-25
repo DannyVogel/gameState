c8fc2f0bc1db55fd2c794626dff63581a71457f7
DannyVogel <danny.hj.vogel@gmail.com>
2023-03-25 10:53:41 +0100

    initial set up for games played list

--- src/components/GamesPlayedList.jsx
+++ src/components/GamesPlayedList.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import GameSearchResult from './GameSearchResult'
 
 export default function GamesPlayedList() {
+  
+  
+  function renderFiveResults(results){
+    return results.map((result, index) => {  
+      if(index >= (num + 0) && index < (num + 3)){
+            return <GameSearchResult 
+                        key={result.id} 
+                        result={result}
+                        handleAddGameToList={addGameToList}
+                    />
+      }})
+  }
+  
   return (
-    <div>GamesPlayedList</div>
+    <div>
+      <GameSearchResult />
+    </div>
   )
 }
