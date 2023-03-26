22f54343751384c7c67515694754859dfa96988d
DannyVogel <danny.hj.vogel@gmail.com>
2023-03-26 17:21:28 +0200

    update game card for reuse with conditional buttons

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,8 +1,74 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {db, ref, remove, update, gameStateDB} from '../firebaseConfig'
 
 export default function GameCard(props) {
-  
+  const [isOnList, setIsOnList] = useState('')
 
+  useEffect(() => {
+    if(props.onList === 'gamesToPlayList'){
+      setIsOnList('gamesToPlayList')
+    } else if(props.onList === 'gamesPlayedList'){
+      setIsOnList('gamesPlayedList')
+    }
+  }, [])
+
+  function addGameToList(e, game){
+    const list = e.target.id
+    const updates = {};
+    updates[`/users/${props.userUID}/${list}/${game.id}`] = [game];
+    update(gameStateDB, updates);
+}
+
+  let buttonContainerElement = ''
+  if(isOnList === ''){
+    buttonContainerElement =       
+      <div className="endResultContainer">
+        <button
+          id='gamesToPlayList' 
+          className="resultButton red btn" onClick={(e) => addGameToList(e, props.result)}
+        >
+          I want to play it
+        </button>
+        <button
+          id='gamesPlayedList'
+          className="resultButton green btn" onClick={(e) => addGameToList(e, props.result)}
+        >
+          I played it
+        </button>
+      </div>
+  } else if (isOnList === 'gamesToPlayList'){
+    buttonContainerElement = 
+    <div className="endResultContainer">
+    <button
+      id={props.result.id}
+      className="resultButton red btn" onClick={removeFromList}
+    >
+      Remove
+    </button>
+    <button
+      id='gamesPlayedList'
+      className="resultButton green btn" onClick={(e) => addGameToList(e, props.result)}
+    >
+      I played it
+    </button>
+  </div>
+  } else if (isOnList === 'gamesPlayedList'){
+    buttonContainerElement = 
+      <div className="endResultContainer">
+        <button
+          id={props.result.id}
+          className="resultButton red btn" onClick={removeFromList}
+        >
+          Remove
+        </button>
+      </div>
+  }
+
+  function removeFromList(e){
+    const gameID = e.target.id
+    const gameRef = ref(db, `gameState/users/${props.userUID}/${isOnList}/${gameID}`)
+    remove(gameRef)
+  }
     
   return (
     <div key={props.result.id} className="resultContainer">
@@ -43,20 +109,7 @@ export default function GameCard(props) {
         </div>
       </div>
 
-      <div className="endResultContainer">
-        <button
-          id='gamesToPlayList' 
-          className="resultButton red btn" onClick={(e) => props.handleAddGameToList(e, props.result)}
-        >
-          I want to play it
-        </button>
-        <button
-          id='gamesPlayedList'
-          className="resultButton green btn" onClick={(e) => props.handleAddGameToList(e, props.result)}
-        >
-          I played it
-        </button>
-      </div>
+      {buttonContainerElement}
 
     </div>
   );
