2df0d736a2137e5f5c26cd01a7ec70db007d7861
DannyVogel <danny.hj.vogel@gmail.com>
2023-03-27 22:53:36 +0200

    revert prev commit for deployment during development

---
+++ src/utility/apikey.js
@@ -0,0 +1,3 @@
+const apiKey = 'b0bf8fc68ea6427fab93a3535a4be903'
+
+export default apiKey
---
+++ src/utility/firebaseConfigDetails.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDvZtRn9B9OgWOIVHnICiq0_eoNreJBUs0",
+    authDomain: "gamestate-327e2.firebaseapp.com",
+    databaseURL: "https://gamestate-327e2-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "gamestate-327e2",
+    storageBucket: "gamestate-327e2.appspot.com",
+    messagingSenderId: "168937433606",
+    appId: "1:168937433606:web:0919320d20ca2f94033187"
+  };
+
+  export default firebaseConfig
