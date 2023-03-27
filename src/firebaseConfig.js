2697f3fa6552113e17c687d440bdf7c1f0263c05
DannyVogel <danny.hj.vogel@gmail.com>
2023-03-27 19:09:05 +0200

    hid firebase config details

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,20 +1,12 @@
 // Import the functions you need from the SDKs you need
+import firebaseConfig from "./utility/firebaseConfigDetails";
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut } from "firebase/auth";
 import { getDatabase, ref, get, child, update, remove, onValue } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration 
-const firebaseConfig = {
-  apiKey: "AIzaSyDvZtRn9B9OgWOIVHnICiq0_eoNreJBUs0",
-  authDomain: "gamestate-327e2.firebaseapp.com",
-  databaseURL: "https://gamestate-327e2-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "gamestate-327e2",
-  storageBucket: "gamestate-327e2.appspot.com",
-  messagingSenderId: "168937433606",
-  appId: "1:168937433606:web:0919320d20ca2f94033187"
-};
+// Your web app's Firebase configuration: imported firebaseconfig
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
