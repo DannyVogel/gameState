6b85dcb5401bfde4b5de2d51d0bc7b33dde5329c
DannyVogel <danny.hj.vogel@gmail.com>
2023-03-28 13:15:07 +0200

    update to include modal with user input when adding to game played list

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -4,7 +4,7 @@ import {db, ref, remove, update, gameStateDB} from '../firebaseConfig'
 export default function GameCard(props) {
   const [isOnList, setIsOnList] = useState('')
   const [userPlayedGameData, setUserPlayedGameData] = useState({
-    yearPlayed: '', comments: ''
+    monthPlayed: '', yearPlayed: '', comments: '', status: ''
   })
   const [showModal, setShowModal] = useState(false)
 
@@ -17,10 +17,10 @@ export default function GameCard(props) {
   }, [])
 
   function handleChange(e){
-    const {name, value} = e.target
+    const {name, value, type, checked} = e.target
     setUserPlayedGameData(prevState => ({
       ...prevState,
-      [name]: value
+      [name]: type === "checkbox" ? checked : value
     }))
 
   }
@@ -50,33 +50,79 @@ export default function GameCard(props) {
 
   let buttonContainerElement = ''
   if(isOnList === ''){
-    buttonContainerElement =       
+    buttonContainerElement = (
       <div className="endResultContainer">
         <button
-          id='gamesToPlayList' 
-          className="resultButton red btn" onClick={(e) => addGameToList(e, props.result)}
+          id="gamesToPlayList"
+          className="resultButton red btn"
+          onClick={(e) => addGameToList(e, props.result)}
         >
           I want to play it
         </button>
         <button
-          id='gamesPlayedList'
-          className="resultButton green btn" onClick={handleShowModal}
+          id="gamesPlayedList"
+          className="resultButton green btn"
+          onClick={handleShowModal}
         >
           I played it
         </button>
-        {showModal 
-          ? <div className="modalContainer">
-              <span className="closeModal" onClick={handleCloseModal}>&times;</span>
-              <form className='modalForm' id={'gamesPlayedList'} onSubmit={(e) => handleSubmit(e, props.result)}>
-                <input type="number" name="yearPlayed" id="yearPlayed" min={1900} max={3000} value={userPlayedGameData.yearPlayed} onChange={handleChange}/>
-                <input type="text" name="comments" id="comments" value={userPlayedGameData.comments} onChange={handleChange}/>
-                <button type="submit">Save</button>
-              </form>
-            </div>
-          : null  
-        }
-            
+        {showModal ? (
+          <div className="modalContainer">
+            <span className="closeModal" onClick={handleCloseModal}>
+              x
+            </span>
+            <form
+              className="modalForm"
+              id={"gamesPlayedList"}
+              onSubmit={(e) => handleSubmit(e, props.result)}
+            >
+              <div className="datePlayed">
+                <span>Date Played:</span> 
+                <input
+                  type="number"
+                  name="monthPlayed"
+                  id="monthPlayed"
+                  min={1}
+                  max={12}
+                  value={userPlayedGameData.monthPlayed}
+                  onChange={handleChange}
+                  placeholder="MM"
+                />-
+                <input
+                  type="number"
+                  name="yearPlayed"
+                  id="yearPlayed"
+                  min={1900}
+                  max={3000}
+                  value={userPlayedGameData.yearPlayed}
+                  onChange={handleChange}
+                  placeholder="YYYY"
+                />
+              </div>
+              <div className="gameCommentsContainer">
+                <label htmlFor="comments">Comments:</label>
+                <textarea
+                  name="comments"
+                  id="comments"
+                  rows={2}
+                  cols={25}
+                  value={userPlayedGameData.comments}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="gameStatusContainer">
+                <span>Game status:</span>
+                <input type="radio" name="status" id="beat" value="beat" checked={userPlayedGameData.status === 'beat'} onChange={handleChange}/>
+                <label htmlFor="beat">Beat</label>
+                <input type="radio" name="status" id="dropped" value="dropped" checked={userPlayedGameData.status === 'dropped'} onChange={handleChange}/>
+                <label htmlFor="dropped">Dropped</label>
+              </div>
+              <button className='addToListBtn btn' type="submit">Save</button>
+            </form>
+          </div>
+        ) : null}
       </div>
+    );
   } else if (isOnList === 'gamesToPlayList'){
     buttonContainerElement = 
     <div className="endResultContainer">
