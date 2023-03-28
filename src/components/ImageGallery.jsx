802512ce39ed7b22f2ec95c4ef0aeefd2a61c394
DannyVogel <danny.hj.vogel@gmail.com>
2023-03-28 15:59:33 +0200

    create image gallery component

---
+++ src/components/ImageGallery.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+
+export default function ImageGallery(props) {
+    const [num, setNum] = useState(0)
+
+    function handleClick(e){
+        const side = e.target.id
+        if(side === 'left'){
+            num > 0 ? setNum(prevNum => prevNum -= 1) : setNum(0)
+        } else if(side === 'right'){
+            num < props.screenshots.length-1 ? setNum(prevNum => prevNum += 1) : setNum(0)
+        }
+    }
+
+    return (
+        <div className="imageGalleryContainer">
+            <div id='left' className='imageSelect left' onClick={handleClick}> {'<'} </div>
+            <img
+                className="image"
+                src={props.screenshots[num]}
+            />
+            <div id='right' className='imageSelect right' onClick={handleClick}> {'>'} </div>
+        </div>
+  )
+}
