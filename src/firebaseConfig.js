3e171b90ea5cbf9d7f8569ed5c61c94b4d26c050
DannyVogel <danny.hj.vogel@gmail.com>
2023-07-08 11:09:16 +0200

    formatting

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,8 +1,22 @@
 // Import the functions you need from the SDKs you need
 import firebaseConfig from "./utility/firebaseConfigDetails";
 import { initializeApp } from "firebase/app";
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut } from "firebase/auth";
-import { getDatabase, ref, get, child, update, remove, onValue } from "firebase/database";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+} from "firebase/auth";
+import {
+  getDatabase,
+  ref,
+  get,
+  child,
+  update,
+  remove,
+  onValue,
+} from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -12,6 +26,20 @@ import { getDatabase, ref, get, child, update, remove, onValue } from "firebase/
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getDatabase(app);
-const gameStateDB = ref(db, "gameState")
+const gameStateDB = ref(db, "gameState");
 
-export {auth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut, db, gameStateDB, ref, get, child, update, remove, onValue}
+export {
+  auth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+  db,
+  gameStateDB,
+  ref,
+  get,
+  child,
+  update,
+  remove,
+  onValue,
+};
