f37224f673d51e10c24ca923b41ba54596aadcbd
DannyVogel <danny.hj.vogel@gmail.com>
2023-07-08 10:47:05 +0200

    cleanup

--- src/components/WelcomeSplash.jsx
+++ src/components/WelcomeSplash.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { db, ref, onValue } from "../firebaseConfig";
 
 export default function WelcomeSplash(props) {
   const UID = props.userUID;
