733ecd0ca8c0197b9f2f51bb3f7f0c0e9b537afd
dvogelg4al <danny.hj.vogel@gmail.com>
2023-11-27 01:29:29 +0100

    refactor: use router

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './App.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Link,
+} from "react-router-dom";
+import Layout from "./layout/main";
+import Home from "./routes/Home";
+import ErrorPage from "./error.page";
+import "./assets/globals.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />} errorElement={<ErrorPage />}>
+      <Route index element={<Home />} />
+      <Route path="*" element={<ErrorPage />} />
+    </Route>
+  )
+);
 
-root.render(
-  <App />
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
