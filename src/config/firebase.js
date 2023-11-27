4de0d9e816b3b14fbf74f449866b2a367bcb04bf
dvogelg4al <danny.hj.vogel@gmail.com>
2023-11-27 01:28:32 +0100

    move files, simplify, init api functions file

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,3 @@
-// Import the functions you need from the SDKs you need
-import firebaseConfig from "./utility/firebaseConfigDetails";
 import { initializeApp } from "firebase/app";
 import {
   getAuth,
@@ -17,12 +15,17 @@ import {
   remove,
   onValue,
 } from "firebase/database";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration: imported firebaseconfig
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASEURL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_FIREBASE_APPID,
+};
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getDatabase(app);
---
+++ src/services/api/firebase.js
@@ -0,0 +1,59 @@
+import {
+  auth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+} from "@/config/firebase";
+
+export const signIn = async (email, password) => {
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      console.log("usercredential", userCredential);
+      return {
+        success: true,
+        user: userCredential.user,
+      };
+    })
+    .catch((error) => {
+      console.log(error.code, error.message);
+      return {
+        success: false,
+        errorCode: errorCode,
+        errorMessage: errorMessage,
+      };
+    });
+};
+function processSignUpFormData(e) {
+  e.preventDefault();
+  setErrorMessage("");
+  createUserWithEmailAndPassword(
+    auth,
+    signUpFormData.signUpEmail,
+    signUpFormData.signUpPassword
+  )
+    .then((userCredential) => {
+      setTimeout(() => {
+        props.handleProfileClick();
+      }, 1500);
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode, errorMessage);
+      // ..
+    });
+}
+function handleSignOut() {
+  signOut(auth)
+    .then(() => {
+      setTimeout(() => {
+        props.handleProfileClick();
+      }, 1000);
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode, errorMessage);
+    });
+}
