09c83cad2d6fb816b243b5c4e542f8b63610b039
dvogelg4al <danny.hj.vogel@gmail.com>
2023-11-28 01:46:33 +0100

    remove prop passing, use store

--- src/layout/main.jsx
+++ src/layout/main.jsx
@@ -1,38 +1,43 @@
+import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { auth, onAuthStateChanged, db, ref } from "../config/firebase";
+import { sliceEmail } from "../utils";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import { NavLink, Outlet } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { auth, onAuthStateChanged, db, ref } from "../config/firebase";
+import useUserStore from "../stores/userStore";
 
 const Layout = () => {
-  const [loggedIn, setLoggedIn] = useState(false);
-  const [user, setUser] = useState("");
+  const setUID = useUserStore((state) => state.setUID);
+  const setUser = useUserStore((state) => state.setUser);
+  const setLogged = useUserStore((state) => state.setLogged);
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       if (user) {
-        const uid = user.uid;
-        setLoggedIn(true);
-        setUser(user.email.slice(0, user.email.indexOf("@")));
+        const { uid, email } = user;
+        console.log("auth state changed", user.uid, user.email);
+        setLogged(true);
+        setUID(uid);
+        setUser(sliceEmail(email));
         const gamesPlayedRef = ref(
           db,
           `gameState/users/${uid}/gamesPlayedList`
         );
       } else {
-        setLoggedIn(false);
+        setLogged(false);
         setUser("");
+        setUID("");
         // User is signed out
       }
     });
   }, []);
   return (
     <div className="container mx-auto p-4 min-h-screen flex flex-col ">
-      <Header loggedIn={loggedIn} user={user} />
-
+      <Header />
       <div className="flex-grow">
         <Outlet />
       </div>
-      <Footer handlePageChange={(e) => handlePageChange(e)} />
+      <Footer />
     </div>
   );
 };
