360caa33cb64b942b8f72bd20d3305c9c62856d8
Danny Vogel <90798411+DannyVogel@users.noreply.github.com>
2023-11-29 17:12:31 +0100

    update styles, refactor, fix close func

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,16 @@
 import Modal from "./Auth/Modal";
-import useUserStore from "../stores/userStore";
+import useUserStore from "@/stores/userStore";
 import { Link } from "react-router-dom";
 import logged from "@/assets/icons/loggedInUser.png";
 import notLogged from "@/assets/icons/loggedOutUser.png";
 
 export default function Header(props) {
   const isLogged = useUserStore((state) => state.isLogged);
-  function handleProfileClick() {
+  function authOpen() {
     document.getElementById("my_modal_5").showModal();
+  }
+  function authClose() {
+    document.getElementById("my_modal_5").close();
   }
 
   return (
@@ -17,28 +20,18 @@ export default function Header(props) {
           <span className="italic">game</span>State
         </Link>
       </h1>
-      {isLogged ? (
-        <img
-          src={logged}
-          alt="profile icon"
-          className="w-8 h-8"
-          onClick={handleProfileClick}
-        />
-      ) : (
-        <img
-          src={notLogged}
-          alt="profile icon"
-          className="w-8 h-8"
-          onClick={handleProfileClick}
-        />
-      )}
+      <img
+        src={isLogged ? logged : notLogged}
+        alt="profile icon"
+        className="w-8 h-8"
+        onClick={authOpen}
+      />
       <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
         <div className="modal-box">
-          <Modal handleProfileClick={handleProfileClick} />
+          <Modal authClose={authClose} />
           <div className="modal-action">
             <form method="dialog">
               {/* if there is a button in form, it will close the modal */}
-              <button className="btn">Close</button>
             </form>
           </div>
         </div>
