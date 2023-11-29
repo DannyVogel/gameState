3215fe139d8e836300e21ffd54d953e02e8c06f2
Danny Vogel <90798411+DannyVogel@users.noreply.github.com>
2023-11-29 09:53:59 +0100

    add styles

--- src/components/Home/Default.jsx
+++ src/components/Home/Default.jsx
@@ -1,43 +1,46 @@
 import played from "@/assets/icons/gamesPlayed.png";
 import toPlay from "@/assets/icons/gamesToPlay.png";
+import search from "@/assets/icons/search.png";
 
 const Default = () => {
   return (
-    <div className="welcomeContainer">
-      <div className="iconContainer">
+    <div className="flex-grow flex flex-col justify-center items-center">
+      <div className="font-bold flex-grow flex flex-col items-center gap-10 text-xl bg-gradient-to-l from-fuchsia-500 via-red-600 to-orange-400 bg-clip-text text-transparent">
         <h1 className="headerTitle">Find Games!</h1>
         <img
           className="w-8 h-8"
-          src="./search.png"
+          src={search}
           alt="magnifying glass search icon"
         />
-      </div>
-      <h1 className="headerTitle">Save them to a list!</h1>
-      <div className="listsContainer">
-        <div className="iconContainer">
-          <img
-            className="w-8 h-8"
-            src={toPlay}
-            alt="shopping bag with controller icon"
-          />
-          <p className="headerTitle">
-            Games <span>{<br />}</span> to play
-          </p>
+        <h1 className="headerTitle">Save them to a list!</h1>
+        <div className=" max-w-md w-full flex justify-around">
+          <div className="flex flex-col items-center gap-2">
+            <img
+              className="w-8 h-8"
+              src={toPlay}
+              alt="shopping bag with controller icon"
+            />
+            <p className="bg-gradient-to-l from-fuchsia-500 via-red-600 to-orange-400 bg-clip-text text-transparent">
+              Games <span>{<br />}</span> to play
+            </p>
+            {/* <p>{props.gamesToPlayList.length} games listed</p> */}
+          </div>
+          <div className="flex flex-col items-center gap-2">
+            <img
+              className="w-8 h-8"
+              src={played}
+              alt="papers with controller icon"
+            />
+            <p className="bg-gradient-to-l from-fuchsia-500 via-red-600 to-orange-400 bg-clip-text text-transparent">
+              Games <span>{<br />}</span> played
+            </p>
+            {/* <p>{props.gamesPlayedList.length} games listed</p> */}
+          </div>
         </div>
-        <div className="iconContainer">
-          <img
-            className="w-8 h-8"
-            src={played}
-            alt="papers with controller icon"
-          />
-          <p className="headerTitle">
-            Games <span>{<br />}</span> played
-          </p>
-        </div>
+        <h1 className="headerTitle">
+          Sign up to see your <span>{<br />}</span> lists across devices!
+        </h1>
       </div>
-      <h1 className="headerTitle">
-        Sign up to see your <span>{<br />}</span> lists across devices!
-      </h1>
     </div>
   );
 };
