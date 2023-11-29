1ff1be429741385f44f4eeb419ae1750f91213bc
Danny Vogel <90798411+DannyVogel@users.noreply.github.com>
2023-11-29 17:17:17 +0100

    update sliceEmail func

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,7 @@
 export const sliceEmail = (email) => {
-  return email.slice(0, email.indexOf("@"));
+  let user = email.slice(0, email.indexOf("@"));
+  if (user == "kelevrav") {
+    user = user.slice(0, -1);
+  }
+  return user.charAt(0).toUpperCase() + user.slice(1);
 };
