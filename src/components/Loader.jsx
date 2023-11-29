4268450d97ff7cbd311aa8a03a524387073426fc
dvogelg4al <danny.hj.vogel@gmail.com>
2023-11-29 08:54:42 +0100

    style loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,18 +2,24 @@ import gameConsole from "../assets/gameConsole.png";
 
 export default function Loader(props) {
   return (
-    <div className={`${props.isUnmounting && ""} loader`}>
-      <div className="loader-wrapper">
-        <h1 className="headerTitle">
-          <span className="gameItalic">game</span>State
+    <div
+      className={`${
+        props.isUnmounting && ""
+      } mt-40 flex justify-center items-center`}
+    >
+      <div className="flex flex-col items-center gap-5">
+        <h1 className="py-1 font-bold text-4xl mx-auto bg-gradient-to-l from-fuchsia-500 via-red-600 to-orange-400 bg-clip-text text-transparent">
+          <span className="italic">game</span>State
         </h1>
         <img
-          className="loader-image"
+          className=""
           src={gameConsole}
           alt="gameConsole enlarged image"
           width={150}
         />
-        <h2 className="headerTitle">Loading...</h2>
+        <h2 className="animate-pulse py-1 font-bold text-4xl mx-auto bg-gradient-to-l from-fuchsia-500 via-red-600 to-orange-400 bg-clip-text text-transparent">
+          Loading...
+        </h2>
       </div>
     </div>
   );
