20cb2a177ca7de4f4b80a3ca88f8bfed8a244deb
Danny Vogel <90798411+DannyVogel@users.noreply.github.com>
2023-11-30 15:32:28 +0100

    update @ alias resolver

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import { fileURLToPath, URL } from "url";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
   plugins: [react()],
