6b4f01c668eb6e0f571a54a3cfe6a58d3bf42126
dvogelg4al <danny.hj.vogel@gmail.com>
2023-12-04 00:11:04 +0100

    init search controller

---
+++ src/services/api/rawg.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import Result from "@/utility/resultsConstructor";
+
+export class RAWGController {
+  static async searchGames(searchTerm) {
+    try {
+      const response = await axios.get(
+        `https://api.rawg.io/api/games?key=${
+          import.meta.env.VITE_RAWG_API_KEY
+        }&search=${searchTerm}`
+      );
+      if (response.status !== 200) {
+        throw new Error("Error: ", response);
+      }
+      return response.data.results.map((result) => new Result(result));
+    } catch (error) {
+      console.log(error);
+      return error;
+    }
+  }
+}
