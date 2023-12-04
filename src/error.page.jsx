7969f932fd6a63bfa6daa9f7d5459c06a3676a6a
dvogelg4al <danny.hj.vogel@gmail.com>
2023-12-04 08:41:02 +0100

    add link to home

--- src/error.page.jsx
+++ src/error.page.jsx
@@ -1,4 +1,5 @@
 import { useRouteError } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function ErrorPage() {
   const error = useRouteError();
@@ -14,6 +15,9 @@ export default function ErrorPage() {
       <p className="text-center">
         <i>Error: {error.statusText || error.message}</i>
       </p>
+      <Link to="/" className="btn btn-primary">
+        Return Home
+      </Link>
     </div>
   );
 }
