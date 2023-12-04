1c136ec6992fa13212d6534864724778efd9a981
Danny Vogel <90798411+DannyVogel@users.noreply.github.com>
2023-12-04 15:17:30 +0100

    init update database functions

--- src/services/api/firestore.js
+++ src/services/api/firestore.js
@@ -1,4 +1,29 @@
-import axios from "axios";
-import Result from "@/utility/resultsConstructor";
+import { db, ref, remove, update, gameStateDB } from "@/config/firebase";
 
-export class FireStoreController {}
+export class FireStoreController {
+  static async addToList(UID, game) {
+    const updates = {};
+    updates[`/users/${UID}/gameList/${game.id}`] = game;
+    update(gameStateDB, updates);
+  }
+
+  static async removeFromList(UID, gameID) {
+    const gameRef = ref(db, `gameState/users/${UID}/gameList/${gameID}`);
+    remove(gameRef);
+  }
+}
+
+// function addGameToList(e, game) {
+//   const list = e.target.id;
+//   const updates = {};
+//   updates[`/users/${UID}/${list}/${game.id}`] = [game];
+//   update(gameStateDB, updates);
+// }
+
+// function removeFromList(e) {
+//   const gameID = e.target.id;
+//   if (gameID) {
+//     const gameRef = ref(db, `gameState/users/${UID}/${isOnList}/${gameID}`);
+//     remove(gameRef);
+//   }
+// }
