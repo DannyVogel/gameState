b870ef61e6b2fd40818eca1bc940633807f93920
dvogelg4al <danny.hj.vogel@gmail.com>
2024-05-19 00:05:23 +0200

    Feat: persist user data in local storage

--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -1,14 +1,22 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-const useUserStore = create((set) => ({
-  isLogged: false,
-  UID: "",
-  user: "",
-  gameList: [],
-  setUser: (user) => set((state) => ({ user: user })),
-  setUID: (id) => set((state) => ({ UID: id })),
-  setLogged: (bool) => set((state) => ({ isLogged: bool })),
-  setGameList: (list) => set((state) => ({ gameList: list })),
-}));
+const useUserStore = create(
+  persist(
+    (set) => ({
+      isLogged: false,
+      UID: "",
+      user: "",
+      gameList: [],
+      setUser: (user) => set((state) => ({ user: user })),
+      setUID: (id) => set((state) => ({ UID: id })),
+      setLogged: (bool) => set((state) => ({ isLogged: bool })),
+      setGameList: (list) => set((state) => ({ gameList: list })),
+    }),
+    {
+      name: "user-storage",
+    }
+  )
+);
 
 export default useUserStore;
