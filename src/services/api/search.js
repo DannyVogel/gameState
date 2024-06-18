1ca3f3d226c0d0c0d1af94322e24776c061344de
Danny <danny.hj.vogel@gmail.com>
2024-06-18 07:33:11 +0200

    Fix: refactor search to request from backend

--- src/services/api/search.js
+++ src/services/api/search.js
@@ -6,12 +6,10 @@ const SERVER_URL = import.meta.env.VITE_SERVER_URL;
 export class SearchController {
   static async search(query) {
     console.log("searching for game");
-    const token = await this.init();
     try {
       const response = await axios.get(`${SERVER_URL}/api/search/${query}`, {
         headers: {
           Accept: "application/json",
-          Authorization: `Bearer ${token}`,
         },
       });
       console.log("search response", response);
